45fef9afad92d59c77a2e805154484e77ae37084
Sumit Saha <Sumit360saha@gmail.com>
2024-10-31 22:32:11 +0530

    Init Changes for checking env

---
+++ hello.java
@@ -0,0 +1,48 @@
+Last login: Thu Oct 31 11:13:51 on ttys000
+sumitsaha@192 ~ % vim hello.java
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class test{
+        public static void main(String[] args){
+                System.out.println("Hello World!");
+}
+}
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+
